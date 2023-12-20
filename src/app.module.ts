1715cda31a0dac2f6decfa04b48e47e52a7168a1
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2023-12-20 14:46:44 -0500

    configure inital app and creat docker DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TareasModule } from './tareas/tareas.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
-  imports: [],
+  imports: [UsuariosModule, TareasModule, CategoriasModule],
   controllers: [AppController],
   providers: [AppService],
 })
