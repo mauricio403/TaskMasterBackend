b68fca7c164e50370f27b276355207c1cb84deb6
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2024-02-09 18:31:30 -0500

    add category apis

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,34 +1,19 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
-import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 
-@Controller('categorias')
+@Controller('categories')
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Post()
-  create(@Body() createCategoriaDto: CreateCategoriaDto) {
+  createCategory(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriasService.create(createCategoriaDto);
   }
 
   @Get()
-  findAll() {
-    return this.categoriasService.findAll();
+  getAllCategories() {
+    return this.categoriasService.getAllCategories();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoriasService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
-    return this.categoriasService.update(+id, updateCategoriaDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriasService.remove(+id);
-  }
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/categoria.entity';
 
 @Injectable()
 export class CategoriasService {
-  create(createCategoriaDto: CreateCategoriaDto) {
-    return 'This action adds a new categoria';
+
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) { }
+
+  async create(createCategoriaDto: CreateCategoriaDto) {
+
+    const { categoryName } = createCategoriaDto;
+
+    const categoryNameExist = await this.getCategoryByName(categoryName);
+
+    if (categoryNameExist) throw new BadRequestException(`Category with name ${categoryName} already exist!`);
+
+    const category = await this.categoryRepository.create({ categoryName });
+
+    try {
+
+      await this.categoryRepository.save(category);
+
+      return {
+        msg: 'Category created successfully',
+        status: '201',
+        data: category
+      }
+
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+
   }
 
-  findAll() {
-    return `This action returns all categorias`;
+  async getAllCategories() {
+
+    const categories = await this.categoryRepository.find();
+
+    if (!categories.length) throw new NotFoundException('Not Found Categories!');
+
+    return {
+      msg: 'OK',
+      code: '200',
+      data: categories
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} categoria`;
+
+  private async getCategoryByName(name: string): Promise<Category> {
+    const category = await this.categoryRepository.findOne({ where: { categoryName: name } });
+    
+    return category ?? undefined;
   }
 
-  update(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
+  private handleDBErrors(error: any) {
+    if (error.detail) {
+      throw new BadRequestException(error.detail)
+    }
+    console.log(error);
+    throw new InternalServerErrorException('Please check internal logs');
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} categoria`;
-  }
+
 }
